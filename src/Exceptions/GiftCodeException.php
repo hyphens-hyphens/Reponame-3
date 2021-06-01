3aa5dbc0177b98b3053f3948e878a034a08e1953
Hai Nguyen <hainguyen@t2gcorp.com>
2021-06-01 14:28:41 +0700

    commit feature/#Improve-Giftcode  * add function check giftcode was used in month

--- src/Exceptions/GiftCodeException.php
+++ src/Exceptions/GiftCodeException.php
@@ -12,15 +12,16 @@ use T2G\Common\Models\GiftCodeItem;
  */
 class GiftCodeException extends \Exception
 {
-    const ERROR_CODE_DISABLE               = 100;
-    const ERROR_CODE_NOT_FOUND             = 101;
-    const ERROR_CODE_USED                  = 102;
-    const ERROR_CODE_API_ERROR             = 103;
-    const ERROR_CODE_CLAIMED               = 104;
-    const ERROR_CODE_NOT_AVAILABLE         = 105;
-    const ERROR_CODE_ISSUER_NOT_MATCH      = 106;
-    const ERROR_ISSUE_CODE_RANGE_NOT_VALID = 107;
-    const ERROR_CODE_PER_MONTH_EXPIRED     = 108;
+    const ERROR_CODE_DISABLE                    = 100;
+    const ERROR_CODE_NOT_FOUND                  = 101;
+    const ERROR_CODE_USED                       = 102;
+    const ERROR_CODE_API_ERROR                  = 103;
+    const ERROR_CODE_CLAIMED                    = 104;
+    const ERROR_CODE_NOT_AVAILABLE              = 105;
+    const ERROR_CODE_ISSUER_NOT_MATCH           = 106;
+    const ERROR_ISSUE_CODE_RANGE_NOT_VALID      = 107;
+    const ERROR_CODE_PER_MONTH_EXPIRED          = 108;
+    const ERROR_CODE_WAS_USED_ONCE_IN_MONTH     = 109;
 
     /**
      * @var \T2G\Common\Models\GiftCodeItem
@@ -45,15 +46,16 @@ class GiftCodeException extends \Exception
     {
         $this->giftCode = $giftCodeItem;
         $messages       = [
-            self::ERROR_CODE_DISABLE               => "This gift code is disabled",
-            self::ERROR_CODE_NOT_FOUND             => "Gift code not found",
-            self::ERROR_CODE_USED                  => "Gift code was used",
-            self::ERROR_CODE_API_ERROR             => "Cannot use code, API server return an error",
-            self::ERROR_CODE_CLAIMED               => "This user already claimed this type of gift code before",
-            self::ERROR_CODE_NOT_AVAILABLE         => "Out of Gift Code for issuing",
-            self::ERROR_CODE_ISSUER_NOT_MATCH      => "This gift code was issued for another user",
-            self::ERROR_ISSUE_CODE_RANGE_NOT_VALID => "The `from` and `to` arguments were not valid",
-            self::ERROR_CODE_PER_MONTH_EXPIRED     => "The gift code was expired",
+            self::ERROR_CODE_DISABLE                    => "This gift code is disabled",
+            self::ERROR_CODE_NOT_FOUND                  => "Gift code not found",
+            self::ERROR_CODE_USED                       => "Gift code was used",
+            self::ERROR_CODE_API_ERROR                  => "Cannot use code, API server return an error",
+            self::ERROR_CODE_CLAIMED                    => "This user already claimed this type of gift code before",
+            self::ERROR_CODE_NOT_AVAILABLE              => "Out of Gift Code for issuing",
+            self::ERROR_CODE_ISSUER_NOT_MATCH           => "This gift code was issued for another user",
+            self::ERROR_ISSUE_CODE_RANGE_NOT_VALID      => "The `from` and `to` arguments were not valid",
+            self::ERROR_CODE_PER_MONTH_EXPIRED          => "The gift code was expired",
+            self::ERROR_CODE_WAS_USED_ONCE_IN_MONTH     => "This type of code was used this month",
 
         ];
 
